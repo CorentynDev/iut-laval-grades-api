acb5aa10420c4fdab2c50bcb0e0bef1d324e43d6
CorentynDev <corentyn.dev@gmail.com>
2025-01-21 19:38:58 +0100

    Update app.ts

--- src/app.ts
+++ src/app.ts
@@ -18,7 +18,7 @@ setupUnhandledErrors();
 const app = express();
 
 app.use(cors({
-    origin: 'http://localhost:5173',
+    origin: '*',
     methods: ['GET', 'POST', 'PUT', 'DELETE', 'OPTIONS'],
     allowedHeaders: ['Content-Type', 'Authorization'],
 }));
